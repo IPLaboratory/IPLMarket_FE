66b1abc7af6a5accb62e37b33dbb80e2bff411e2
HwangSB <hsb990105@gmail.com>
2023-08-09 14:04:58 +0900

    TASK #3 : FragmentManager 재사용 가능 하도록 수정 FragmentManager가 setFrag 함수 안에서 불필요하게 재생성되는 코드가 있어 onCreate에서 생성하도록 수정했습니다.

--- app/src/main/java/com/example/iplmarket_fe/MainActivity.java
+++ app/src/main/java/com/example/iplmarket_fe/MainActivity.java
@@ -1,9 +1,7 @@
 package com.example.iplmarket_fe;
 
 import android.os.Bundle;
-import android.view.MenuItem;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
@@ -25,21 +23,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        fm = getSupportFragmentManager();
+
         // 바텀 네비게이션 뷰
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavi);
-        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                int itemId = item.getItemId();
-                if (itemId == R.id.action_create) {
-                    setFrag(0);
-                } else if (itemId == R.id.action_home) {
-                    setFrag(1);
-                } else if (itemId == R.id.action_settings) {
-                    setFrag(2);
-                }
-                return true;
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            int itemId = item.getItemId();
+            if (itemId == R.id.action_create) {
+                setFrag(0);
+            } else if (itemId == R.id.action_home) {
+                setFrag(1);
+            } else if (itemId == R.id.action_settings) {
+                setFrag(2);
             }
+            return true;
         });
 
         createfrag = new CreateFrag();
@@ -50,7 +47,6 @@ public class MainActivity extends AppCompatActivity {
 
     // 프래그먼트 교체가 일어나는 실행문
     private void setFrag(int n) {
-        fm = getSupportFragmentManager();
         ft = fm.beginTransaction();
         switch (n) {
             case 0:
